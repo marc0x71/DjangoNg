e2272fd174f97770ed84beae743041c73791150b
marc0x71 <marc0x71@yahoo.it>
2024-04-06 16:38:28 +0200

    Aggiunto ExampleModel a interfaccia admin di django

--- backend/mybackend/my_app/admin.py
+++ backend/mybackend/my_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from my_app.models import ExampleModel
 
 # Register your models here.
+admin.site.register(ExampleModel)
--- backend/mybackend/my_app/models.py
+++ backend/mybackend/my_app/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class ExampleModel(models.Model):
 	firstname    = models.CharField(max_length=200)
 	lastname     = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.lastname + ", " + self.firstname
